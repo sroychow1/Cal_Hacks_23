fa6a8813863e476703ad8e89d425c9cc38b53da9
Andres Nedilskyj <an3developer@gmail.com>
2023-10-28 03:05:38 -0700

    added 10 second get request

--- web_scraper.py
+++ web_scraper.py
@@ -3,6 +3,7 @@ import asyncio
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 import Hume_sentiment_analysis as hume
+import math
 
 import requests
 
@@ -19,11 +20,14 @@ async def start():
     data = result.json()
     # print(data[0]["text"])
 
-    for i in range(len(data)):
+    for i in range(3):
         tweet = data[i]
         text = tweet["text"]
         score = await hume.run_hume(text)
         print("score: " + str(score) + " text: \"" + text + "\"")
+    
+    await asyncio.sleep(10)
+    await start()
     
 
 asyncio.run(start())
