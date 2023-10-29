c7428ae3a00431bfd31f9f7d508538da2c40e6ed
Aarush Das <aarushdas@berkeley.edu>
2023-10-28 23:05:06 -0700

    converts hume scores to vector

--- Hume_sentiment_analysis.py
+++ Hume_sentiment_analysis.py
@@ -1,17 +1,19 @@
 import asyncio
+import numpy as np
 
 from hume import HumeStreamClient, StreamSocket
 from hume.models.config import LanguageConfig
 
 async def main():
-    client = HumeStreamClient("1cJ51ZOtKPkG4GXbPjZoHESZOo2ou2IMTUS1lLBFgHa4pASm")
+    client = HumeStreamClient("6zhPTYAeccmYjFwgfCMIwXbdtszFAuKn6sT5HdSSAbIWmQyu")
     config = LanguageConfig()
     async with client.connect([config]) as socket:
-        result = await socket.send_file("/Users/shoumikroychowdhury/Cal_Hacks_23/test.text")
+        result = await socket.send_file("test.txt")
         emotions = result['language']['predictions'][0]['emotions']
-        sorted_emotions = sorted(emotions, key=lambda x: x['score'], reverse=True)
-        top_10_emotions = [item['name'] for item in sorted_emotions[:10]]
-        print(emotions)
+        sorted_emotions = sorted(emotions, key=lambda x: x['name'])
+        vector_vals = [item['score'] for item in sorted_emotions]
+        vector = np.array(vector_vals)
+        return np.transpose(vector)
 
 
 asyncio.run(main())
